32e089ee83cad96d59537cc4efe5a3d4e7f88ac8
olatunde222 <olatunde_olukoya@yahoo.com>
2024-02-22 20:30:14 +0400

    second commit 22/02/24 8:28pm

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,14 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
-//FONTS CONFIGURATION// 
-const IBMPlex = IBM_Plex_Sans({ 
+//FONTS CONFIGURATION//
+const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
-  weight: ["400", "500", "600","700"],
-  variable: '--font-ibm-plex'
- });
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex",
+});
 
 export const metadata: Metadata = {
   title: "Imaginify Saas App",
@@ -21,8 +22,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
